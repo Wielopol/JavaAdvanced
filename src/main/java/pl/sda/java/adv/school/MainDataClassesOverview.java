d11a84b95b6504510ddeca3e222294a6704da131
Wielopol <karolwiel@interia.pl>
2022-02-09 12:34:01 +0100

    added sorting

--- src/main/java/pl/sda/java/adv/school/MainDataClassesOverview.java
+++ src/main/java/pl/sda/java/adv/school/MainDataClassesOverview.java
@@ -2,7 +2,9 @@ package pl.sda.java.adv.school;
 
 import pl.sda.java.adv.school.model.Student;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MainDataClassesOverview {
@@ -10,23 +12,23 @@ public class MainDataClassesOverview {
     public static void main(String[] args) {
         Student student1 = new Student();
 
-        student1.setFirstName("Michał");
+        student1.setFirstName("Michal");
         student1.setLastName("Zielinski");
 
         Student student2 = new Student();
 
-        student2.setFirstName("Bogdan");
-        student2.setLastName("Czarny");
+        student2.setFirstName("Bozydar");
+        student2.setLastName("Filar");
 
         Student student3 = new Student();
 
-        student3.setFirstName("Bożydar");
+        student3.setFirstName("Bogdan");
         student3.setLastName("Filar");
 
         Student student4 = new Student();
 
         student4.setFirstName("Amadeusz");
-        student4.setLastName("Tyrała");
+        student4.setLastName("Tyrala");
 
         System.out.println("\nLet's have an array we can iterate over");
 
@@ -57,5 +59,14 @@ public class MainDataClassesOverview {
         System.out.println("\nLet's use stream on array");
 
         Arrays.stream(studentsArray).forEach(System.out::println);
+
+        System.out.println("\nLet's sort the list");
+
+        //we need to create the ArrayList because result of List.of(...) is NOT mutable!
+        List<Student> studentsArrayList = new ArrayList<>(studentsList);
+
+        Collections.sort(studentsArrayList);
+
+        studentsArrayList.forEach(System.out::println);
     }
 }
