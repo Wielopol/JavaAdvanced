eaa1c5d8efa91e94c2a9c72d045113d1d6d4ad7f
Wielopol <karolwiel@interia.pl>
2022-02-13 19:44:17 +0100

    Corrected sorting by age ascending

--- src/main/java/pl/sda/java/adv/school/MainStudentsLoader.java
+++ src/main/java/pl/sda/java/adv/school/MainStudentsLoader.java
@@ -42,19 +42,15 @@ public class MainStudentsLoader {
                 } else {
                     if (monthResult > 0) {
                         return -1;
-                    } else if(monthResult < 0) {
-                        return 1;
                     } else {
-                        return 0;
+                        return 1;
                     }
                 }
             } else {
                 if (yearResult > 0) {
                     return -1;
-                } else if(yearResult < 0) {
-                    return 1;
                 } else {
-                    return 0;
+                    return 1;
                 }
             }
         }));
